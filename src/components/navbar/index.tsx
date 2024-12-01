c262b99316fe886c40b6c05ce6f6a5b95f4fee1b
MichalJaniec <michal.janiec95@gmail.com>
2024-12-01 21:47:43 +0100

    creatiing draft for navbar component

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,38 @@
+import { ActiveLink } from "@/components/active-link";
+import { Route } from "next";
+
+type NavLink = {
+	href: Route;
+	title: string;
+};
+
+const NAV_LINKS: NavLink[] = [
+	{
+		href: "/",
+		title: "Home",
+	},
+	{
+		href: "/skills",
+		title: "Skills",
+	},
+	{
+		href: "/experience",
+		title: "Experience",
+	},
+	{
+		href: "/projects",
+		title: "Projects",
+	},
+];
+
+export const Navbar = () => {
+	return (
+		<nav className="mb-4 md:mb-0">
+			<ul className="flex space-x-4">
+				{NAV_LINKS.map((link) => (
+					<ActiveLink key={link.href} href={link.href} title={link.title} exact={false} />
+				))}
+			</ul>
+		</nav>
+	);
+};
