05674ebe40dad9fbf8b82e80a7885031aa547740
MichalJaniec <michal.janiec95@gmail.com>
2024-12-02 11:54:49 +0100

    adding possibility to show border

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -5,11 +5,12 @@ type ImageProps = {
 	alt: string;
 	width?: number;
 	height?: number;
+	showBorder?: boolean;
 };
 
-export const Img = ({ src, alt, width, height }: ImageProps) => {
+export const Img = ({ src, alt, width, height, showBorder }: ImageProps) => {
 	return (
-		<div className="mb-8 md:mb-0 md:w-1/2">
+		<div className="relative mb-8">
 			<Image
 				src={src}
 				alt={alt}
@@ -17,6 +18,9 @@ export const Img = ({ src, alt, width, height }: ImageProps) => {
 				height={height || 500}
 				className="rounded-lg shadow-md"
 			/>
+			{showBorder && (
+				<div className="absolute inset-0 rounded-lg border-4 border-b-purple-500 border-l-blue-500 border-r-purple-500 border-t-blue-500"></div>
+			)}
 		</div>
 	);
 };
