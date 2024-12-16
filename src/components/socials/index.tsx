e9d3cf098e51e28e807febdb8476845d775d0502
MichalJaniec <michal.janiec95@gmail.com>
2024-12-16 23:06:31 +0100

    impr icon, remove repating code

--- src/components/socials/index.tsx
+++ src/components/socials/index.tsx
@@ -1,14 +1,32 @@
-import { Github, LinkedinIcon } from "lucide-react";
+"use client";
+import { FaGithub, FaLinkedin, FaStrava } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
+const SOCIALS = [
+	{
+		href: "#",
+		icon: <FaLinkedin />,
+	},
+	{
+		href: "#",
+		icon: <FaGithub />,
+	},
+	{
+		href: "#",
+		icon: <FaStrava />,
+	},
+];
 
 export const Socials = () => {
 	return (
-		<section className="flex space-x-4">
-			<a href="#">
-				<LinkedinIcon className="h-6 w-6 text-gray-600 hover:text-blue-500" />
-			</a>
-			<a href="#">
-				<Github className="h-6 w-6 text-gray-600 hover:text-blue-500" />
-			</a>
-		</section>
+		<IconContext.Provider value={{ size: "32px" }}>
+			<section className="mt-16 flex space-x-4">
+				{SOCIALS.map((social) => (
+					<a className="text-gray-600 hover:text-blue-500" href={social.href}>
+						{social.icon}
+					</a>
+				))}
+			</section>
+		</IconContext.Provider>
 	);
 };
