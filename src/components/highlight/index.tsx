1d8c683843e884470f2172e3fadbaf30107d24c3
MichalJaniec <michal.janiec95@gmail.com>
2024-12-17 00:03:32 +0100

    impr line size

--- src/components/highlight/index.tsx
+++ src/components/highlight/index.tsx
@@ -9,7 +9,7 @@ export const Highlight = ({ children, hideLine, className }: HighlightProps) => 
 		<span className={`relative inline-block font-semibold ${className ? className : ""}`}>
 			{children}
 			{!hideLine && (
-				<span className="absolute inset-x-0 bottom-0 h-[2px] origin-left scale-x-100 rounded-lg bg-blue-600 transition-transform duration-300"></span>
+				<span className="absolute inset-x-0 bottom-0 h-[4px] origin-left scale-x-100 rounded-lg bg-blue-600 transition-transform duration-300"></span>
 			)}
 		</span>
 	);
