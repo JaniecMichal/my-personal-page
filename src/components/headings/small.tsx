396b982f29a7964409612b9c73498e72e4653f08
MichalJaniec <michal.janiec95@gmail.com>
2024-12-16 23:06:51 +0100

    add small variant for heading

---
+++ src/components/headings/small.tsx
@@ -0,0 +1,5 @@
+import type { HeadingProps } from "./types";
+
+export const SmallHeading = ({ text }: HeadingProps) => {
+	return <h2 className="md:text-md font-regular mb-4 text-xl text-gray-800 lg:text-2xl">{text}</h2>;
+};
