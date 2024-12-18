d63c3ef03236bf118894141fb6e61ef8bfc214cb
MichalJaniec <michal.janiec95@gmail.com>
2024-12-18 23:22:17 +0100

    add description to page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -24,7 +24,12 @@ export default async function MyProjects() {
 					</>
 				}
 			/>
-			<Description>List of my projects</Description>
+			<Description>
+				Below is a list of my projects. These include projects I've built from the ground up, as
+				well as those where I actively participated as a member of a development team. Through
+				dedicated work on these projects, I've gained valuable knowledge and experience that I
+				leverage in my daily work.
+			</Description>
 			<Projects />
 		</section>
 	);
