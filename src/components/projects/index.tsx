6ebf621d7713adf32ef5920969fa15de3b4a7f6a
MichalJaniec <michal.janiec95@gmail.com>
2025-02-05 22:08:48 +0100

    add key to project list

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -28,7 +28,11 @@ export const Projects = ({ projects }: { projects: ProjectListItemFragment[] }) 
 		<PageSectionLayout sectionTitle="My Projects">
 			<ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
 				{projects.map((project) => (
-					<ProjectListItem project={project} onProjectClick={() => handleOpenModal(project)} />
+					<ProjectListItem
+						key={project.id}
+						project={project}
+						onProjectClick={() => handleOpenModal(project)}
+					/>
 				))}
 			</ul>
 
