d29c5c97da5ebdd14113e5b005df0f39acfa77c7
MichalJaniec <michal.janiec95@gmail.com>
2025-04-03 00:15:55 +0200

    hide contact form until fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -93,13 +93,13 @@ export default function Contact() {
             </motion.div>
           </div>
 
-          <motion.div
+          {/* <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.6 }}
           >
             <ContactForm />
-          </motion.div>
+          </motion.div> */}
         </div>
       </section>
     </div>
